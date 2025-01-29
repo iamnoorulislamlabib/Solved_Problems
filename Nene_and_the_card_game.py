deca53e0881a9d69dd46f4e9a4a2f52a76a8c126
Labib <iamnoorulislamlabib@gmail.com>
2025-01-29 22:22:48 +0600

    800

---
+++ Nene_and_the_card_game.py
@@ -0,0 +1,44 @@
+# https://codeforces.com/problemset/problem/1956/B
+
+import sys
+import os
+
+def is_online_judge():
+    return os.getenv("ONLINE_JUDGE") is not None  # Some OJs set this
+
+
+def redirect_io():
+    """Redirects I/O only if running locally."""
+    if not is_online_judge():  
+        try:
+            sys.stdin = open("input.txt", "r")
+            sys.stdout = open("output.txt", "w")
+        except FileNotFoundError:
+            pass
+
+
+
+
+def solve():
+    _ = input()
+    lst = list(map(int,input().split()))
+    ans = 0
+    dictionary = {}
+    for num in lst :
+        if dictionary.get(num,0) == 0 :
+            dictionary[num] = 0
+        dictionary[num] += 1
+        if dictionary[num] > 1 :
+            ans += 1
+    print(ans)
+        
+        
+
+
+if __name__ == "__main__":
+    redirect_io()  # Redirect only for local runs
+
+    test_cases = 1
+    test_cases = int(input())
+    for tc in range(test_cases) :
+        solve()
